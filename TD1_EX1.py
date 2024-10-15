2c3ce25051f2bb81d0f70c9847a525944a8baaa4
Valouuuuuu <155658885+Valouuuuuu@users.noreply.github.com>
2024-10-15 15:51:51 +0200

    Add files via upload

---
+++ TD1_EX1.py
@@ -0,0 +1,8 @@
+prenom = "Valentin"
+nom = "Launay"
+math = 11
+anglais = 2
+info = 16
+promotion = 2024
+
+print(prenom, nom," a eu ", math,"en maths", anglais,"en anglais", info,"en informatique","il a commencer sa formation en",  promotion)
